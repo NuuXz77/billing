623e26593357c18601f9a1a6fcab7b2a8f9a0101
nuu <wisnu@gmail.com>
2025-11-11 12:14:39 +0700

    tes auto deploy

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,4 +1,4 @@
 {{-- //memakai komponen layout guest --}}
 <x-layouts.guest>
-    <h1 class="text-center font-bold text-2xl">LANDING PAGE</h1>
+    <h1 class="text-center font-bold text-2xl">BILLING CIHUY</h1>
 </x-layouts.guest>

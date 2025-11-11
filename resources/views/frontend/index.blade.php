ae5f869d3e51105530a13b2cca8eb3ed8cb410b1
nuu <wisnu@gmail.com>
2025-11-11 16:10:54 +0700

    tes auto deploy

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,4 +1,4 @@
 {{-- //memakai komponen layout guest --}}
 <x-layouts.guest>
-    <h1 class="text-center font-bold text-2xl">BILLING CIHOI</h1>
+    <h1 class="text-center font-bold text-2xl">BILLING HUUUUUUUUU</h1>
 </x-layouts.guest>
